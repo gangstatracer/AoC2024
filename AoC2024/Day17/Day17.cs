bea1e0e39efb38862493df12b4a6f1bacf5967cf
Ivan Perevoschikov <gangstatracer@gmail.com>
2024-12-18 10:25:20 +0500

    WIP

--- AoC2024/Day17/Day17.cs
+++ AoC2024/Day17/Day17.cs
@@ -29,7 +29,7 @@ public class Day17
         string.Join(',', machine.Output).Should().Be(expected);
     }
 
-    [Ignore]
+    [Ignore("long running")]
     [TestCase("Day17/example2.txt", 117440)]
     [TestCase("Day17/input.txt", 0)]
     public void Task2(string filePath, int expected)
