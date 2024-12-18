6dfa29ce8133cf362d6539ac5cd303e25656f156
Ivan Perevoschikov <gangstatracer@gmail.com>
2024-12-18 14:23:10 +0500

    Add debug logging

--- AoC2024/Day18/Day18.cs
+++ AoC2024/Day18/Day18.cs
@@ -3,7 +3,7 @@ namespace AoC2024.Day18;
 public class Day18
 {
     [TestCase("Day18/example.txt", 12, 7, 22)]
-    [TestCase("Day18/input.txt", 1024, 71, 0)]
+    // [TestCase("Day18/input.txt", 1024, 71, 0)]
     public void Task1(string filePath, int bytesFallen, int mapSize, int expected)
     {
         var bytes = File
@@ -12,8 +12,8 @@ public class Day18
         {
             var parts = l.Split(',');
             return new Coordinate(
-                int.Parse(parts[0]), 
-                int.Parse(parts[1]));
+                int.Parse(parts[1]), 
+                int.Parse(parts[0]));
         })
         .ToArray();
 
@@ -23,6 +23,8 @@ public class Day18
 
         for(var i = 0; i < bytesFallen; i++)
             memory.TrySetValue(bytes[i], true);
+
+        memory.Print(TestContext.Out);
 
         var q = new Queue<Coordinate>();
         
@@ -43,8 +45,9 @@ public class Day18
                 q.Enqueue(n);
             }
         }
+        distances.Print(TestContext.Out);
         // Search(memory, distances, new Coordinate(0,0), 0);
-        distances[mapSize -1, mapSize -1].Should().Be(expected);
+        distances[mapSize - 1, mapSize - 1].Should().Be(expected);
     }
 
     private static void Search(bool[,] memory, int[,] distances, Coordinate c, int distance)
