d21ab8aeef3bd5cdc352eb9202846d60ab3fde64
Ivan Perevoschikov <gangstatracer@gmail.com>
2024-12-18 14:27:01 +0500

    add my input

--- AoC2024/Day18/Day18.cs
+++ AoC2024/Day18/Day18.cs
@@ -3,7 +3,7 @@ namespace AoC2024.Day18;
 public class Day18
 {
     [TestCase("Day18/example.txt", 12, 7, 22)]
-    // [TestCase("Day18/input.txt", 1024, 71, 0)]
+    [TestCase("Day18/input.txt", 1024, 71, 0)]
     public void Task1(string filePath, int bytesFallen, int mapSize, int expected)
     {
         var bytes = File
@@ -24,7 +24,7 @@ public class Day18
         for(var i = 0; i < bytesFallen; i++)
             memory.TrySetValue(bytes[i], true);
 
-        memory.Print(TestContext.Out);
+        // memory.Print(TestContext.Out);
 
         var q = new Queue<Coordinate>();
         
@@ -45,7 +45,7 @@ public class Day18
                 q.Enqueue(n);
             }
         }
-        distances.Print(TestContext.Out);
+        // distances.Print(TestContext.Out);
         // Search(memory, distances, new Coordinate(0,0), 0);
         distances[mapSize - 1, mapSize - 1].Should().Be(expected);
     }
